635fc774387bd62e37b10a0dd77005cb8a9a8729
Pine Mizune <pinemz@gmail.com>
2015-11-01 17:05:08 +0900

    Refactor shards builder

--- lib/Crenv/Builder/Shards.pm
+++ lib/Crenv/Builder/Shards.pm
@@ -8,20 +8,21 @@ use Cwd qw/abs_path/; # >= perl 5
 use Crenv::Utils;
 
 sub new {
-    my ($class, %opt) = @_;
-    bless { %opt } => $class;
+    my $class = shift;
+    bless {} => $class;
 }
 
 sub build {
     my ($self, $target_dir, $crystal_dir) = @_;
 
-    $self->_install_libyaml;
+    $self->install_libyaml($target_dir);
 
     my $crystal_bin      = abs_path("$crystal_dir/bin/crystal");
     my $env_crystal_path = abs_path("$crystal_dir/libs").':.';
 
     my $command = <<"EOF";
 CRYSTAL_PATH=$env_crystal_path \
+LD_LIBRARY_PATH=$target_dir:\$LD_LIBRARY_PATH \
 cd "$target_dir" && "$crystal_bin" build --release src/shards.cr -o bin/shards
 EOF
 
@@ -31,17 +32,42 @@ EOF
     return "$target_dir/bin/shards";
 }
 
-sub _install_libyaml {
-    my ($platform, $arch) = Crenv::Utils::system_info();
+sub install_libyaml {
+    my ($self, $target_dir) = @_;
 
+    my ($platform) = Crenv::Utils::system_info();
     if ($platform eq 'darwin') {
-        return unless system('which brew > /dev/null 2>&1') == 0;
-
-        if (system('brew list libyaml > /dev/null 2>&1') != 0) {
-            system('brew install libyaml');
-        }
+        $self->install_libyaml_with_brew($target_dir);
     }
 }
 
+sub install_libyaml_with_brew {
+    my ($self, $target_dir) = @_;
+
+    unless (system('which brew > /dev/null 2>&1') == 0) {
+        warn 'You should install Homebrew';
+        return;
+    }
+
+    my $prefix = $self->fetch_libyaml_prefix_with_brew;
+    unless (-d $prefix) {
+        system('brew install libyaml');
+        $prefix = $self->fetch_libyaml_prefix_with_brew;
+    }
+
+    my $libyaml_path = File::Spec->join($prefix, "lib/libyaml.a");
+    if (-f $libyaml_path) {
+        system("cp -f \"$libyaml_path\" \"$target_dir/libyaml.a\"");
+    }
+}
+
+sub fetch_libyaml_prefix_with_brew {
+    my $self = shift;
+
+    my $prefix = `brew --prefix libyaml 2>/dev/null`;
+    chomp $prefix;
+
+    return $prefix;
+}
 
 1;
