4d9efc08d4213ccc1c0eb7eca804c700a30f09e9
Pine Mizune <pinemz@gmail.com>
2016-11-21 19:35:14 +0900

    fix(Mac/OSVersion): upgrade version

--- vendor/lib/Mac/OSVersion/Lite.pm
+++ vendor/lib/Mac/OSVersion/Lite.pm
@@ -3,10 +3,11 @@ use strict;
 use warnings;
 use utf8;
 
-our $VERSION = "0.02";
+our $VERSION = "0.04";
 
 use constant VERSION_FORMAT    => qr/(?<major>[0-9]+)(?:\.(?<minor>[0-9]+))?(?:\.(?<point>[0-9]+))?/;
 use constant MAC_VERSION_NAMES => {
+    sierra        => "10.12",
     el_capitan    => "10.11",
     yosemite      => "10.10",
     mavericks     => "10.9",
